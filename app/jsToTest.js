d59595e16339af46e5e23c6006d996cb787f30c2
marcin <marcin@marcins-iMac.local>
2016-04-29 19:10:49 +0200

    Initial commit

---
+++ app/jsToTest.js
@@ -0,0 +1,13 @@
+/**
+ * Created by marcin on 29/04/16.
+ */
+window.dog = {
+    name: "reks",
+
+    bark: function () {
+        return "hauuu";
+    },
+    walk: function () {
+        return "hop hop hop";
+    }
+}
---
+++ spec/trueTest.js
@@ -0,0 +1,5 @@
+describe("Dog test", function () {
+    it("dog should bark with hauuu", function () {
+        expect(dog.bark()).toBe("hauuu")
+    })
+})
